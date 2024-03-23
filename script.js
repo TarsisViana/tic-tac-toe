d8562df5eaf647a2f4d1527b54924d558196f105
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-03-23 16:39:34 +0000

    Make the game work in the console
    used 2 factory functions to make the game board and display it, and the
    other to handle the game logic.

---
+++ script.js
@@ -0,0 +1,116 @@
+const gameBoard = (function(){
+
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  //make the board with obj
+  for(let i = 0; i < rows; i++){
+    board[i] = [];
+
+    for(let j = 0; j < columns; j++){
+      board[i].push(cell());
+    }
+  }
+  
+  //for the console version
+  function print(){
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  }
+
+  const getBoard = () => board;
+
+  const makePlay = (playerToken,row,collumn) => {
+    //check if its marked
+    if(board[row][collumn].getValue()) return;
+
+    //if not update the cell value;
+    board[row][collumn].getPlay(playerToken);
+  }
+
+  function cell(){
+    let value = undefined;
+  
+    function getPlay (playerToken) { 
+      value = playerToken;
+    }
+  
+    const getValue = () => value;
+    return {getPlay, getValue}
+  }
+
+  const checkForWinner = () =>{
+    //short for currentBoard
+    const cBoard = board.map((row) => row.map((cell) => cell.getValue()))
+    let winner = false;
+
+    for(let i = 0; i < 3; i++){
+      //check rows
+      let row = cBoard[i];
+      if(row[0] === row[1] && row[0] === row[2] && row[0] !== undefined){
+        winner = true;
+        break;
+      }
+      //check columns
+      if(
+        cBoard[0][i] === cBoard[1][i] && 
+        cBoard[0][i] === cBoard[2][i] &&
+        cBoard[0][i] !== undefined
+        )
+        {
+          winner = true;
+          break;
+      }
+    }
+
+    return winner;
+  }
+  
+  return {print, getBoard, makePlay, checkForWinner}
+})();
+
+
+
+function gameController(playerOne = 'Player One',playerTwo = 'Player Two'){
+
+
+  const players = [
+    {name: playerOne, token: 'x'},
+    {name: playerTwo, token: 'o'}
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewround = () => {
+    console.log(gameBoard.print());
+    console.log(`${activePlayer.name}'s turn...` );
+  }
+
+  const playRound = (row,column) => {
+    gameBoard.makePlay(activePlayer.token, row, column)
+    let winner;
+    //check for a winner after every play
+    winner = gameBoard.checkForWinner(activePlayer);
+    if(!winner){
+      switchPlayer();
+      printNewround();
+      return;
+    }
+    printNewround();
+    console.log(`${activePlayer.name} is the winner!`)
+  }
+
+
+  printNewround();
+
+  return {playRound};
+}
+
+const game = gameController()
